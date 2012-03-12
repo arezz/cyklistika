dffa792497d52d388a474b6ce24658d53ac30fe8
Vojtech Gruchala <gruchala@raynet.cz>
2012-03-12 20:17:31 +0100

    / utils-funkce (z prihlaseni)

--- prihlaseni-zavodnika.php
+++ prihlaseni-zavodnika.php
@@ -1,7 +1,7 @@
 <?php
   require("nastaveni.php");
   include("save.php");
-  include("prihlaseni-funkce.php");
+  include("utils-funkce.php");
   echo "<?xml version='1.0' encoding='windows-1250' ?>"; ?> 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">  
