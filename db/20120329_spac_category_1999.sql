109e8eca367b52c9f8ed9e69dd0b63a68e8bda58
arezz <v.gruchala@centrum.cz>
2012-03-29 23:28:07 +0200

    + spac category 1999 + all csv

---
+++ db/20120329_spac_category_1999.sql
@@ -0,0 +1,4 @@
+INSERT INTO `spac_category`(`id`,`prefix`,`name`,`coefficient`,`season`) VALUES ( '10','A','Muži 18 až 35 let','1.00','1999');
+INSERT INTO `spac_category`(`id`,`prefix`,`name`,`coefficient`,`season`) VALUES ( '11','B','Muži 36 až 45 let','1.00','1999');
+INSERT INTO `spac_category`(`id`,`prefix`,`name`,`coefficient`,`season`) VALUES ( '12','C','Muži 46 až 55 let','1.00','1999');
+INSERT INTO `spac_category`(`id`,`prefix`,`name`,`coefficient`,`season`) VALUES ( '13','D','Muži nad 56 let','1.00','1999');
