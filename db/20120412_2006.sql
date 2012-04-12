514518d1d46ab1530ecd6e7ada2fa4538bd09bcf
Vojtech Gruchala <gruchala@raynet.cz>
2012-04-12 23:55:11 +0200

    @ oprava roku 2006

---
+++ db/20120412_2006.sql
@@ -0,0 +1,55 @@
+﻿/*
+SQLyog Enterprise - MySQL GUI v8.05 
+MySQL - 5.0.51b-community-nt : Database - spac-stats
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+/*Data for the table `spac_results_2006` */
+
+TRUNCATE TABLE `spac_results_2006`;
+
+insert  into `spac_results_2006`(`id`,`id_racer`,`surname`,`firstname`,`team`,`id_category`,`race_1`,`race_2`,`race_3`,`race_4`,`race_5`,`total`,`final_standing`,`total_racers`) values (1,188,'Mosiol','Norbert','SK ŠAFRATA BOHUMÍN',1,4,4,0,8,8,24,1,13);
+insert  into `spac_results_2006`(`id`,`id_racer`,`surname`,`firstname`,`team`,`id_category`,`race_1`,`race_2`,`race_3`,`race_4`,`race_5`,`total`,`final_standing`,`total_racers`) values (2,34,'UVÍRA','Václav','CK ORLÍK ORLOVÁ',1,8,0,0,4,0,12,2,13);
+insert  into `spac_results_2006`(`id`,`id_racer`,`surname`,`firstname`,`team`,`id_category`,`race_1`,`race_2`,`race_3`,`race_4`,`race_5`,`total`,`final_standing`,`total_racers`) values (3,782,'OGIERMAN','Michal','SK ŠAFRATA BOHUMÍN',1,6,0,0,0,6,12,3,13);
+insert  into `spac_results_2006`(`id`,`id_racer`,`surname`,`firstname`,`team`,`id_category`,`race_1`,`race_2`,`race_3`,`race_4`,`race_5`,`total`,`final_standing`,`total_racers`) values (4,25,'MALOŠÍK','Jan','CK ORLÍK ORLOVÁ',1,0,1,0,6,4,11,4,13);
+insert  into `spac_results_2006`(`id`,`id_racer`,`surname`,`firstname`,`team`,`id_category`,`race_1`,`race_2`,`race_3`,`race_4`,`race_5`,`total`,`final_standing`,`total_racers`) values (5,754,'KOLESÁR','Radek','TJ KOLÍK BOHUMÍN',1,0,8,0,0,0,8,5,13);
+insert  into `spac_results_2006`(`id`,`id_racer`,`surname`,`firstname`,`team`,`id_category`,`race_1`,`race_2`,`race_3`,`race_4`,`race_5`,`total`,`final_standing`,`total_racers`) values (6,242,'SWACZYNA','Petr','CK FESO PETŘVALD',1,0,6,0,0,0,6,6,13);
+insert  into `spac_results_2006`(`id`,`id_racer`,`surname`,`firstname`,`team`,`id_category`,`race_1`,`race_2`,`race_3`,`race_4`,`race_5`,`total`,`final_standing`,`total_racers`) values (7,17,'UBLANSKÝ','Adam','CK ORLÍK ORLOVÁ',1,0,2,0,3,1,6,7,13);
+insert  into `spac_results_2006`(`id`,`id_racer`,`surname`,`firstname`,`team`,`id_category`,`race_1`,`race_2`,`race_3`,`race_4`,`race_5`,`total`,`final_standing`,`total_racers`) values (8,751,'RYLICH','Robin','CK ORLÍK ORLOVÁ',1,0,3,0,0,1,4,8,13);
+insert  into `spac_results_2006`(`id`,`id_racer`,`surname`,`firstname`,`team`,`id_category`,`race_1`,`race_2`,`race_3`,`race_4`,`race_5`,`total`,`final_standing`,`total_racers`) values (9,718,'BORSZICKÝ','Denis','CK ORLÍK ORLOVÁ',1,0,1,0,0,3,4,9,13);
+insert  into `spac_results_2006`(`id`,`id_racer`,`surname`,`firstname`,`team`,`id_category`,`race_1`,`race_2`,`race_3`,`race_4`,`race_5`,`total`,`final_standing`,`total_racers`) values (10,10,'Foltyn','Jiří','SK ŠAFRATA BOHUMÍN',1,2,1,0,0,1,4,10,13);
+insert  into `spac_results_2006`(`id`,`id_racer`,`surname`,`firstname`,`team`,`id_category`,`race_1`,`race_2`,`race_3`,`race_4`,`race_5`,`total`,`final_standing`,`total_racers`) values (11,742,'KOVAŘÍK','David','SK JIŘÍ TEAM OSTRAVA',1,3,0,0,0,0,3,11,13);
+insert  into `spac_results_2006`(`id`,`id_racer`,`surname`,`firstname`,`team`,`id_category`,`race_1`,`race_2`,`race_3`,`race_4`,`race_5`,`total`,`final_standing`,`total_racers`) values (12,1091,'JURÁK','Jakub','TJ BANÍK HAVÍŘOV',1,0,0,0,0,2,2,12,13);
+insert  into `spac_results_2006`(`id`,`id_racer`,`surname`,`firstname`,`team`,`id_category`,`race_1`,`race_2`,`race_3`,`race_4`,`race_5`,`total`,`final_standing`,`total_racers`) values (13,1092,'NOVOTNÝ','Rostislav','CK ORLÍK ORLOVÁ',1,0,0,0,0,1,1,13,13);
+insert  into `spac_results_2006`(`id`,`id_racer`,`surname`,`firstname`,`team`,`id_category`,`race_1`,`race_2`,`race_3`,`race_4`,`race_5`,`total`,`final_standing`,`total_racers`) values (14,71,'GIRACZKA','Martin','TJ KOLÍK BOHUMÍN',2,8,8,0,8,8,32,1,10);
+insert  into `spac_results_2006`(`id`,`id_racer`,`surname`,`firstname`,`team`,`id_category`,`race_1`,`race_2`,`race_3`,`race_4`,`race_5`,`total`,`final_standing`,`total_racers`) values (15,399,'FAJKUS','Martin','SK ŠAFRATA BOHUMÍN',2,4,4,0,1,4,13,2,10);
+insert  into `spac_results_2006`(`id`,`id_racer`,`surname`,`firstname`,`team`,`id_category`,`race_1`,`race_2`,`race_3`,`race_4`,`race_5`,`total`,`final_standing`,`total_racers`) values (16,599,'RYLICH','Václav','CK ORLÍK ORLOVÁ',2,0,6,0,4,0,10,3,10);
+insert  into `spac_results_2006`(`id`,`id_racer`,`surname`,`firstname`,`team`,`id_category`,`race_1`,`race_2`,`race_3`,`race_4`,`race_5`,`total`,`final_standing`,`total_racers`) values (17,1093,'KOPAČKA','Petr','SK ŠAFRATA BOHUMÍN',2,6,0,0,0,0,6,4,10);
+insert  into `spac_results_2006`(`id`,`id_racer`,`surname`,`firstname`,`team`,`id_category`,`race_1`,`race_2`,`race_3`,`race_4`,`race_5`,`total`,`final_standing`,`total_racers`) values (18,46,'KUBALA','Janusz','SK ŠAFRATA BOHUMÍN',2,0,0,0,6,0,6,5,10);
+insert  into `spac_results_2006`(`id`,`id_racer`,`surname`,`firstname`,`team`,`id_category`,`race_1`,`race_2`,`race_3`,`race_4`,`race_5`,`total`,`final_standing`,`total_racers`) values (19,1094,'HARTIG','David','SK ŠAFRATA BOHUMÍN',2,0,0,0,0,6,6,6,10);
+insert  into `spac_results_2006`(`id`,`id_racer`,`surname`,`firstname`,`team`,`id_category`,`race_1`,`race_2`,`race_3`,`race_4`,`race_5`,`total`,`final_standing`,`total_racers`) values (20,555,'SÝKORA','Miroslav','SK ŠAFRATA BOHUMÍN',2,0,0,0,3,0,3,7,10);
+insert  into `spac_results_2006`(`id`,`id_racer`,`surname`,`firstname`,`team`,`id_category`,`race_1`,`race_2`,`race_3`,`race_4`,`race_5`,`total`,`final_standing`,`total_racers`) values (21,1095,'SOUKUP','Pavel','RSA TEAM',2,0,0,0,2,0,2,8,10);
+insert  into `spac_results_2006`(`id`,`id_racer`,`surname`,`firstname`,`team`,`id_category`,`race_1`,`race_2`,`race_3`,`race_4`,`race_5`,`total`,`final_standing`,`total_racers`) values (22,881,'KRAJC','Radan','KARVINÁ',2,0,0,0,1,0,1,9,10);
+insert  into `spac_results_2006`(`id`,`id_racer`,`surname`,`firstname`,`team`,`id_category`,`race_1`,`race_2`,`race_3`,`race_4`,`race_5`,`total`,`final_standing`,`total_racers`) values (23,992,'TUREČEK','Martin','KARVINÁ',2,0,0,0,1,0,1,10,10);
+insert  into `spac_results_2006`(`id`,`id_racer`,`surname`,`firstname`,`team`,`id_category`,`race_1`,`race_2`,`race_3`,`race_4`,`race_5`,`total`,`final_standing`,`total_racers`) values (24,69,'ZOK','Bogdan','SK ŠAFRATA BOHUMÍN',3,8,8,0,8,8,32,1,7);
+insert  into `spac_results_2006`(`id`,`id_racer`,`surname`,`firstname`,`team`,`id_category`,`race_1`,`race_2`,`race_3`,`race_4`,`race_5`,`total`,`final_standing`,`total_racers`) values (25,210,'VÍTEČEK','Petr','TJ KOLÍK BOHUMÍN',3,0,6,0,0,6,12,2,7);
+insert  into `spac_results_2006`(`id`,`id_racer`,`surname`,`firstname`,`team`,`id_category`,`race_1`,`race_2`,`race_3`,`race_4`,`race_5`,`total`,`final_standing`,`total_racers`) values (26,105,'BAIER','Jan','TJ KOLÍK BOHUMÍN',3,6,0,0,0,0,6,3,7);
+insert  into `spac_results_2006`(`id`,`id_racer`,`surname`,`firstname`,`team`,`id_category`,`race_1`,`race_2`,`race_3`,`race_4`,`race_5`,`total`,`final_standing`,`total_racers`) values (27,1020,'LEPŠÍK','Miroslav','KARVINÁ',3,0,0,0,6,0,6,4,7);
+insert  into `spac_results_2006`(`id`,`id_racer`,`surname`,`firstname`,`team`,`id_category`,`race_1`,`race_2`,`race_3`,`race_4`,`race_5`,`total`,`final_standing`,`total_racers`) values (28,73,'VODIČKA','Štěpán','CYKLOSPORT PŘIKRYL, ST.VES N.O.',3,4,0,0,0,0,4,5,7);
+insert  into `spac_results_2006`(`id`,`id_racer`,`surname`,`firstname`,`team`,`id_category`,`race_1`,`race_2`,`race_3`,`race_4`,`race_5`,`total`,`final_standing`,`total_racers`) values (29,1096,'HAVELKA','Pavel','KARVINÁ',3,0,0,0,4,0,4,6,7);
+insert  into `spac_results_2006`(`id`,`id_racer`,`surname`,`firstname`,`team`,`id_category`,`race_1`,`race_2`,`race_3`,`race_4`,`race_5`,`total`,`final_standing`,`total_racers`) values (30,1097,'STADTHERR','Radim','SK ŠAFRATA BOHUMÍN',3,0,0,0,3,0,3,7,7);
+insert  into `spac_results_2006`(`id`,`id_racer`,`surname`,`firstname`,`team`,`id_category`,`race_1`,`race_2`,`race_3`,`race_4`,`race_5`,`total`,`final_standing`,`total_racers`) values (31,121,'KUBÍK','Josef','TJ KOLÍK BOHUMÍN',4,0,8,0,0,6,14,1,3);
+insert  into `spac_results_2006`(`id`,`id_racer`,`surname`,`firstname`,`team`,`id_category`,`race_1`,`race_2`,`race_3`,`race_4`,`race_5`,`total`,`final_standing`,`total_racers`) values (32,118,'TEŠNAR','Tomáš','YOGI RACING OSTRAVA',4,8,0,0,0,0,8,2,3);
+insert  into `spac_results_2006`(`id`,`id_racer`,`surname`,`firstname`,`team`,`id_category`,`race_1`,`race_2`,`race_3`,`race_4`,`race_5`,`total`,`final_standing`,`total_racers`) values (33,633,'SIKORA','Petr','SK ŠAFRATA BOHUMÍN',4,0,0,0,0,8,8,3,3);
+insert  into `spac_results_2006`(`id`,`id_racer`,`surname`,`firstname`,`team`,`id_category`,`race_1`,`race_2`,`race_3`,`race_4`,`race_5`,`total`,`final_standing`,`total_racers`) values (34,1062,'PETŘÍČKOVÁ','Alena','CK ORLÍK ORLOVÁ',9,0,0,8,8,8,24,1,5);
+insert  into `spac_results_2006`(`id`,`id_racer`,`surname`,`firstname`,`team`,`id_category`,`race_1`,`race_2`,`race_3`,`race_4`,`race_5`,`total`,`final_standing`,`total_racers`) values (35,145,'UBLANSKÁ','Kamila','CK ORLÍK ORLOVÁ',9,0,8,0,4,6,18,2,5);
+insert  into `spac_results_2006`(`id`,`id_racer`,`surname`,`firstname`,`team`,`id_category`,`race_1`,`race_2`,`race_3`,`race_4`,`race_5`,`total`,`final_standing`,`total_racers`) values (36,1098,'GNOJKOVÁ','Katrin','SK ŠAFRATA BOHUMÍN',9,0,4,0,0,4,8,3,5);
+insert  into `spac_results_2006`(`id`,`id_racer`,`surname`,`firstname`,`team`,`id_category`,`race_1`,`race_2`,`race_3`,`race_4`,`race_5`,`total`,`final_standing`,`total_racers`) values (37,1099,'SZWAJNOCHOVÁ','Soňa','SK ŠAFRATA BOHUMÍN',9,0,6,0,0,0,6,4,5);
+insert  into `spac_results_2006`(`id`,`id_racer`,`surname`,`firstname`,`team`,`id_category`,`race_1`,`race_2`,`race_3`,`race_4`,`race_5`,`total`,`final_standing`,`total_racers`) values (38,144,'HALMOVÁ','Eva','CK ORLÍK ORLOVÁ',9,0,0,0,6,0,6,5,5);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
