c8d6a93de9b0a35d5fef068bf1b0d199f52537f8
Vojtech Gruchala <gruchala@raynet.cz>
2012-04-17 23:57:46 +0200

    + kompletni zaloha db se vsemi daty

--- nastaveni.php
+++ nastaveni.php
@@ -1,10 +1,19 @@
 <?php
-$SQL_Server = "mysql.webzdarma.cz";
-$SQL_Uzivatel = "igmk";
-$SQL_Heslo = "lolipopx7";
-$Databaze = "igmk";
+//$SQL_Server = "mysql.webzdarma.cz";
+//$SQL_Uzivatel = "igmk";
+//$SQL_Heslo = "lolipopx7";
+//$Databaze = "igmk";
+//$pripoj=MySQL_Connect($SQL_Server, $SQL_Uzivatel, $SQL_Heslo) or Die(MySQL_Error());
+//MySQL_Select_Db($Databaze) or Die(MySQL_Error());
+//MySQL_Query("SET NAMES 'cp1250'");
+//session_start();
+
+$SQL_Server = "localhost";
+$SQL_Uzivatel = "root";
+$SQL_Heslo = "root";
+$Databaze = "spac-stats";
 $pripoj=MySQL_Connect($SQL_Server, $SQL_Uzivatel, $SQL_Heslo) or Die(MySQL_Error());
 MySQL_Select_Db($Databaze) or Die(MySQL_Error());
 MySQL_Query("SET NAMES 'cp1250'");
-session_start(); 
+session_start();
 ?>
