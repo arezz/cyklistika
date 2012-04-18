2416b27ecd6d361d4b7021b32336b0a50025986c
Vojtech Gruchala <gruchala@raynet.cz>
2012-04-19 00:08:58 +0200

    + nove info/index

--- index.php
+++ index.php
@@ -1,25 +1,100 @@
 <?php
   require("nastaveni.php");
-  echo "<?xml version='1.0' encoding='windows-1250' ?>\n";
-?>
+  echo "<?xml version='1.0' encoding='windows-1250' ?>"; ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
   <head>
     <meta http-equiv="content-type" content="text/html; charset=windows-1250" />
     <meta name="generator" content="PSPad editor, www.pspad.com" />
-    <title>SPAC - přihlašování na závody a statistiky</title>
+    <title>SPAC - přihlašování a statistiky</title>
     <link rel="stylesheet" type="text/css" href="style.css" />
   </head>
   <body>
     <div class="container">
-      <div class="menu">        
+      <div class="menu">
         <?php include("menu.php"); ?>
       </div>
       <div class="content">
+          <table>
+              <tr>
+                  <td>&nbsp;</td>
+                  <td colspan="2"><center><h2>Informace o projektu "SPAC - statistiky"</h2></center></td>
+                  <td>&nbsp</td>
+              </tr>
+            <tr>
+                <td><a href="http://www.spac-os.cz" target="blank"><img src="pics/logo_spac_orange.png"></img></a></td>
+                  <td colspan="2">
+                     Projekt vznikl jako podpora pro <strong>Slezský pohár amatérských cyklistů</strong>.
+                       Primárně byl určen pro <strong>přihlašování na závody</strong>, avšak možnosti
+                       php webu ve spojení s databází a spoustou dat k dispozici
+                       mě motivovalo si s aplikací trošku pohrát. Výsledkem
+                       je kompletní interaktivní <strong>archiv výsledků závodů SPAC od prvního
+                       ročníku z roku 1998</strong>, včetně <strong>profilů jednotlivých
+                       závodníků.</strong>
+                  </td>
+                  <td><a href="http://www.spac-os.cz" target="blank"><img src="pics/logo_spac_orange.png"></img></a></td>
+            </tr>
+            <tr>
+                  <td>&nbsp;</td>
+                  <td colspan="2">
+                     V následující době plánuji udržovat data aktuální a z nových výsledků
+                     vytřískat ještě něco navíc, jako třeba <strong>přehled oddílů (družstev) a
+                     seznam členů</strong> (a jak do týmu bodovali na závodech
+                     i celkem).
+                  </td>
+                  <td>&nbsp;</td>
+            </tr>
+               <tr>
+                  <td>&nbsp;</td>
+                  <td colspan="2">
+                    Samozřejmě jako každý projekt, ani tento není bez chyb. Pokud tedy 
+                    nějakou nesrovnalost objevíte, neváhejte mi napsat na mail
+                    <a href="mailto:v.gruchala@gmail.com">v.gruchala@gmail.com</a>.
+                    Stejně tak uvítám nápady a podněty pro další obsah stránek.
+                  </td>
+                  <td>&nbsp;</td>
+            </tr>
+            <tr>
+                  <td>&nbsp;</td>
+            </tr>
+           <tr>
+                  <td>&nbsp;</td>
+                  <td colspan="2">
+                       Autorem jednoduché architektury je <strong>Jakub Jaroš</strong>, za což mu
+                       ještě jednou chci poděkovat, protože PHP a html nejsou mou silnou
+                       stránkou. Celý web jsem již však převzal já (Vojta Gruchala) a jsem jeho jediným
+                       administrátorem i programátorem.
+                  </td>
+                  <td>&nbsp;</td>
+            </tr>
+            <tr>
+                  <td>&nbsp;</td>
+            </tr>
+           <tr>
+                  <td>&nbsp;</td>
+                  <td colspan="2">
+                      Poslední poděkování patří firmě  <strong><a href="http://www.raynet.cz" target="blank">RAYNET s.r.o.</a></strong>,
+                      u které teď běží databáze (stále zdarma, ale asi 100x
+                      rychleji než u původního poskytovatele).
+                  </td>
+                  <td>&nbsp;</td>
+            </tr>
+            <tr>
+                  <td>&nbsp;</td>
+            </tr>
+              <tr>
+                  <td>&nbsp;</td>
+                  <td  colspan="2">
+                      
+                  </td>                  
+                  <td>&nbsp;</td>
+            </tr>
+
+       </table>
       </div>
       <div class="paticka">
         <?php include("paticka.php"); ?>
-      </div>          
+      </div>
     </div>
   </body>
 </html>
--- menu.php
+++ menu.php
@@ -1,7 +1,10 @@
-<a href="vypis-zavodu-2012.php">Seznam závodù SPAC 2012</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+<h2>
+<a href="vypis-zavodu-2012.php">SPAC 2012</a>&nbsp;&nbsp;|&nbsp;&nbsp;
 <a href="prihlaseni-zavodnika.php">Pøihlášení na závod</a>&nbsp;&nbsp;|&nbsp;&nbsp;
 <a href="statistiky.php">Statistiky</a>&nbsp;&nbsp;|&nbsp;&nbsp;
-<a href="http://www.spac-os.cz/">Stránky SPAC</a>
+<a href="index.php">Info</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+<a href="http://www.spac-os.cz/" target="blank">Stránky SPAC</a>
 <?php if ($_SESSION["administrace"] == 1) 
 {echo"&nbsp;&nbsp;|&nbsp;&nbsp;<a href='logout.php'>Odhlásit se</a>";} 
 ?>
+</h2>
