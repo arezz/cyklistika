314d9d7916cde08082a288d738dca5989faee075
Vojtech Gruchala <gruchala@raynet.cz>
2012-05-17 22:55:58 +0200

    + diskuze

---
+++ diskuze.php
@@ -0,0 +1,29 @@
+<?php
+  require("nastaveni.php");
+  echo "<?xml version='1.0' encoding='windows-1250' ?>"; ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=windows-1250" />
+    <meta name="generator" content="PSPad editor, www.pspad.com" />
+    <title>SPAC - přihlašování a statistiky</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+  </head>
+  <body>
+    <div class="container">
+      <div class="menu">
+        <?php include("menu.php"); ?>
+      </div>
+      <div class="content">
+          <center>
+          <iframe src="http://www.blueboard.cz/kniha/us1mka4knrn6u159f188sho86srr1r"></iframe>
+          <!--<a href="http://miniaplikace.blueboard.cz/kniha/us1mka4knrn6u159f188sho86srr1r">Návštěvní kniha</a>-->
+          </center>
+      </div>
+      <div class="paticka">
+        <?php include("paticka.php"); ?>
+      </div>
+    </div>
+  </body>
+</html>
+<?php MySQL_Close($pripoj); ?>
--- menu.php
+++ menu.php
@@ -1,9 +1,10 @@
 <h2>
-<a href="vypis-zavodu-2012.php">SPAC 2012</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+<a href="vypis-zavodu-2012.php">Sezóna 2012</a>&nbsp;&nbsp;|&nbsp;&nbsp;
 <a href="prihlaseni-zavodnika.php">Pøihlášení na závod</a>&nbsp;&nbsp;|&nbsp;&nbsp;
 <a href="statistiky.php">Statistiky</a>&nbsp;&nbsp;|&nbsp;&nbsp;
 <a href="index.php">Info</a>&nbsp;&nbsp;|&nbsp;&nbsp;
-<a href="http://www.spac-os.cz/" target="blank">Stránky SPAC</a>
+<a href="diskuze.php">Diskuze</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+<a href="http://www.spac-os.cz/" target="blank">SPAC</a>
 <?php if ($_SESSION["administrace"] == 1) 
 {echo"&nbsp;&nbsp;|&nbsp;&nbsp;<a href='logout.php'>Odhlásit se</a>";} 
 ?>
